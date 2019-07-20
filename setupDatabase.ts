f3f74c18f5093d36e29744cdb0d2d2a76bb08f76
Bruno Quaresma <bruno_nonato_quaresma@hotmail.com>
2019-07-20 20:23:19 -0300

    Add setup for the roles

--- setupDatabase.ts
+++ setupDatabase.ts
@@ -40,6 +40,104 @@ const setup = async () => {
       source: q.Collection("Notes")
     })
   );
+
+  await client.query(
+    q.CreateRole({
+      name: "can_manage_self_notes",
+      privileges: [
+        {
+          resource: q.Collection("Notes"),
+          actions: {
+            read: q.Query(
+              q.Lambda(
+                "ref",
+                q.Equals(
+                  q.Identity(),
+                  q.Select(["data", "owner"], q.Get(q.Var("ref")))
+                )
+              )
+            ),
+            write: q.Query(
+              q.Lambda(
+                ["oldData", "newData"],
+                q.And(
+                  q.Equals(
+                    q.Identity(),
+                    q.Select(["data", "owner"], q.Var("oldData"))
+                  ),
+                  q.Equals(
+                    q.Select(["data", "owner"], q.Var("oldData")),
+                    q.Select(["data", "owner"], q.Var("newData"))
+                  )
+                )
+              )
+            ),
+            create: q.Query(
+              q.Lambda(
+                "newData",
+                q.Equals(
+                  q.Identity(),
+                  q.Select(["data", "owner"], q.Var("newData"))
+                )
+              )
+            ),
+            delete: q.Query(
+              q.Lambda(
+                "ref",
+                q.Equals(
+                  q.Identity(),
+                  q.Select(["data", "owner"], q.Get(q.Var("ref")))
+                )
+              )
+            ),
+            history_read: false,
+            history_write: false
+          }
+        },
+        {
+          resource: q.Index("notes_by_owner"),
+          actions: {
+            unrestricted_read: false,
+            read: true,
+            history_read: false
+          }
+        }
+      ],
+      membership: [
+        {
+          resource: q.Collection("Users")
+        }
+      ]
+    })
+  );
+
+  await client.query(
+    q.CreateRole({
+      name: "can_sign_in_and_up",
+      privileges: [
+        {
+          resource: q.Collection("Users"),
+          actions: {
+            read: false,
+            write: false,
+            create: true,
+            delete: false,
+            history_read: false,
+            history_write: false
+          }
+        },
+        {
+          resource: q.Index("user_by_email"),
+          actions: {
+            unrestricted_read: false,
+            read: true,
+            history_read: false
+          }
+        }
+      ],
+      membership: []
+    })
+  );
 };
 
 setup();
