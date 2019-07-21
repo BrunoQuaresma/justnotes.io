6a7a8a6171886c4f1c3424c55f44e5d03206d44a
Bruno Quaresma <bruno_nonato_quaresma@hotmail.com>
2019-07-20 21:11:30 -0300

    Add delete modal confirmation

--- src/Notes.tsx
+++ src/Notes.tsx
@@ -11,7 +11,9 @@ import {
   UncontrolledDropdown,
   DropdownToggle,
   DropdownMenu,
-  DropdownItem
+  DropdownItem,
+  Modal,
+  ModalBody
 } from "reactstrap";
 import { runQuery } from "./database";
 import {
@@ -39,7 +41,10 @@ const Notes: React.FC<RouteComponentProps> = ({ navigate }) => {
   const [notes, setNotes] = useState<Note[] | null>(null);
   const [selectedNoteId, setSelectedNoteId] = useState<string | null>(null);
   const [textareaValue, setTextareavalue] = useState<string>("");
+  const [isDeleting, setIsDeleting] = useState(false);
   const [isCreating, setIsCreating] = useState(false);
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+  const [noteIdToDelete, setNoteIdToDelete] = useState<string | null>(null);
   const hasNotes = useMemo(() => notes && notes.length > 0, [notes]);
 
   const sortedNotes = useMemo(() => {
@@ -130,19 +135,32 @@ const Notes: React.FC<RouteComponentProps> = ({ navigate }) => {
     event.stopPropagation();
   }, []);
 
+  const handleDeleteModalToggle = useCallback(() => {
+    setIsDeleteModalOpen(!isDeleteModalOpen);
+  }, [isDeleteModalOpen]);
+
   const handleNoteDelete = useCallback(
     async (note: Note, event: React.MouseEvent) => {
       event.stopPropagation();
-
-      if (!notes) return;
-
-      await runQuery(DELETE_NOTE(note.ref.id));
-
-      if (selectedNoteId === note.ref.id) setSelectedNoteId(null);
-      setNotes(notes.filter(noteFilter => noteFilter.ref.id !== note.ref.id));
+      setNoteIdToDelete(note.ref.id);
+      handleDeleteModalToggle();
     },
-    [notes, selectedNoteId]
+    [handleDeleteModalToggle]
   );
+
+  const handleDeleteModalConfirm = useCallback(async () => {
+    setIsDeleting(true);
+
+    if (!notes || !noteIdToDelete) return;
+
+    await runQuery(DELETE_NOTE(noteIdToDelete));
+
+    if (selectedNoteId === noteIdToDelete) setSelectedNoteId(null);
+    setNotes(notes.filter(noteFilter => noteFilter.ref.id !== noteIdToDelete));
+    handleDeleteModalToggle();
+    setNoteIdToDelete(null);
+    setIsDeleting(false);
+  }, [handleDeleteModalToggle, noteIdToDelete, notes, selectedNoteId]);
 
   const handleLogout = useCallback(() => {
     logout();
@@ -152,106 +170,133 @@ const Notes: React.FC<RouteComponentProps> = ({ navigate }) => {
   if (!sortedNotes) return <div>Loading...</div>;
 
   return (
-    <div className="notes container-fluid">
-      <div className="row">
-        <div className="scrollable-y col-md-4 col-lg-3 bg-light p-3">
-          <button
-            onClick={handleNewClick}
-            className="btn btn-block btn-primary mb-3"
-            disabled={isCreating}
-          >
-            {isCreating ? "Creating..." : "New note"}
-          </button>
-
-          {sortedNotes.map(note => (
-            <div
-              onClick={() => handleNoteClick(note)}
-              className={`note card mb-1 ${note.ref.id === selectedNoteId &&
-                "note--active"}`}
-              key={note.ref.id}
+    <>
+      <div className="notes container-fluid">
+        <div className="row">
+          <div className="scrollable-y col-md-4 col-lg-3 bg-light p-3">
+            <button
+              onClick={handleNewClick}
+              className="btn btn-block btn-primary mb-3"
+              disabled={isCreating}
             >
-              <div className="card-body">
-                <div className="note__options">
-                  <UncontrolledDropdown>
-                    <DropdownToggle
-                      color="light"
-                      size="sm"
-                      onClick={handleDropdownClick}
-                    >
-                      <i className="fas fa-ellipsis-h" />
-                    </DropdownToggle>
-                    <DropdownMenu right>
-                      <DropdownItem header>Actions</DropdownItem>
-                      <DropdownItem
-                        className="text-danger"
-                        onClick={event => handleNoteDelete(note, event)}
-                      >
-                        Remove
-                      </DropdownItem>
-                    </DropdownMenu>
-                  </UncontrolledDropdown>
-                </div>
-
-                <p className="note__content">
-                  {note.data.content || (
-                    <span className="text-muted">No content</span>
-                  )}
-                </p>
-
-                <small className="note__info text-muted">
-                  {format(note.ts / 1000)}
-                </small>
-              </div>
-            </div>
-          ))}
-        </div>
-
-        <div className="d-md-flex flex-column col-md-8 col-lg-9 p-3">
-          <div className="row px-3 py-1 mb-3 justify-content-end">
-            <button className="btn btn-sm btn-outline-primary mr-1">
-              Share note
+              {isCreating ? "Creating..." : "New note"}
             </button>
 
-            <UncontrolledDropdown>
-              <DropdownToggle outline color="secondary" size="sm" caret>
-                My account
-              </DropdownToggle>
-              <DropdownMenu right>
-                <DropdownItem onClick={handleLogout}>Logout</DropdownItem>
-              </DropdownMenu>
-            </UncontrolledDropdown>
+            {sortedNotes.map(note => (
+              <div
+                onClick={() => handleNoteClick(note)}
+                className={`note card mb-1 ${note.ref.id === selectedNoteId &&
+                  "note--active"}`}
+                key={note.ref.id}
+              >
+                <div className="card-body">
+                  <div className="note__options">
+                    <UncontrolledDropdown>
+                      <DropdownToggle
+                        color="light"
+                        size="sm"
+                        onClick={handleDropdownClick}
+                      >
+                        <i className="fas fa-ellipsis-h" />
+                      </DropdownToggle>
+                      <DropdownMenu right>
+                        <DropdownItem header>Actions</DropdownItem>
+                        <DropdownItem
+                          className="text-danger"
+                          onClick={event => handleNoteDelete(note, event)}
+                        >
+                          Remove
+                        </DropdownItem>
+                      </DropdownMenu>
+                    </UncontrolledDropdown>
+                  </div>
+
+                  <p className="note__content">
+                    {note.data.content || (
+                      <span className="text-muted">No content</span>
+                    )}
+                  </p>
+
+                  <small className="note__info text-muted">
+                    {format(note.ts / 1000)}
+                  </small>
+                </div>
+              </div>
+            ))}
           </div>
 
-          {selectedNoteId ? (
-            <textarea
-              ref={textAreaRef}
-              value={textareaValue}
-              onChange={handleTextChange}
-              placeholder="Type your note here..."
-            />
-          ) : (
-            <div className="notes__empty h-100 d-flex align-items-center justify-content-center">
-              <div className="text-center">
-                <p className="display-3 text-muted">
-                  {hasNotes ? "Select a note" : "Start use notes"}
-                </p>
+          <div className="d-md-flex flex-column col-md-8 col-lg-9 p-3">
+            <div className="row px-3 py-1 mb-3 justify-content-end">
+              <button className="btn btn-sm btn-outline-primary mr-1">
+                Share note
+              </button>
 
-                {!hasNotes && (
-                  <button
-                    onClick={handleNewClick}
-                    className="btn btn-primary"
-                    type="button"
-                    disabled={isCreating}
-                  >
-                    {isCreating ? "Creating..." : "Create your first note"}
-                  </button>
-                )}
-              </div>
+              <UncontrolledDropdown>
+                <DropdownToggle outline color="secondary" size="sm" caret>
+                  My account
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem onClick={handleLogout}>Logout</DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
             </div>
-          )}
+
+            {selectedNoteId ? (
+              <textarea
+                ref={textAreaRef}
+                value={textareaValue}
+                onChange={handleTextChange}
+                placeholder="Type your note here..."
+              />
+            ) : (
+              <div className="notes__empty h-100 d-flex align-items-center justify-content-center">
+                <div className="text-center">
+                  <p className="display-3 text-muted">
+                    {hasNotes ? "Select a note" : "Start use notes"}
+                  </p>
+
+                  {!hasNotes && (
+                    <button
+                      onClick={handleNewClick}
+                      className="btn btn-primary"
+                      type="button"
+                      disabled={isCreating}
+                    >
+                      {isCreating ? "Creating..." : "Create your first note"}
+                    </button>
+                  )}
+                </div>
+              </div>
+            )}
+          </div>
         </div>
       </div>
-    </div>
+
+      <Modal isOpen={isDeleteModalOpen} toggle={handleDeleteModalToggle}>
+        <ModalBody>
+          <div className="p-4">
+            <div className="text-center mb-3 lead">
+              Do you really want to delete this note?
+            </div>
+            <div className="text-center">
+              <button
+                className="btn text-muted"
+                onClick={handleDeleteModalToggle}
+              >
+                Cancel
+              </button>
+              <button
+                onClick={handleDeleteModalConfirm}
+                className="btn btn-outline-danger"
+                disabled={isDeleting}
+              >
+                {isDeleting ? "Deleting..." : "Delete note"}
+              </button>
+            </div>
+          </div>
+        </ModalBody>
+      </Modal>
+    </>
   );
 };
 
