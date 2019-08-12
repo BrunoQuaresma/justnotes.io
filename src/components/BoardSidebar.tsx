1a2f5ac8d16a55611a8024769965fc1daaefefa7
Bruno Quaresma <bruno_nonato_quaresma@hotmail.com>
2019-08-12 17:39:21 -0300

    Display reversed notes

--- src/components/BoardSidebar.tsx
+++ src/components/BoardSidebar.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { useSelector } from 'react-redux'
 import { selectNotes, NoteItem } from 'stores/noteStore'
 import NoteCard from 'components/NoteCard'
@@ -6,12 +6,16 @@ import NewNoteButton from 'components/NewNoteButton'
 
 const BoardSidebar: React.FC = () => {
   const notes: NoteItem[] = useSelector(selectNotes)
+  const reverseNotes = useMemo(
+    () => notes.concat().sort((a, b) => b.ts - a.ts),
+    [notes]
+  )
 
   return (
     <div className="scrollable-y col-md-4 col-lg-3 bg-light p-3">
       <NewNoteButton label="New note" block></NewNoteButton>
 
-      {notes.map(note => (
+      {reverseNotes.map(note => (
         <NoteCard note={note} key={note.id}></NoteCard>
       ))}
     </div>
